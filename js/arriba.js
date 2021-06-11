6ac31c2cea1afa883e558436593a41f5324b742d
jhonkophler <juanadriancorso94@gmail.com>
2021-06-11 12:24:33 -0300

    Prueba Page

---
+++ js/arriba.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $('.irArriba').click(function(){
+        $('body,html').animate({
+            scrollTop: '0px'
+        },10);
+    });
+
+    
+    $(window).scroll(function(){
+        if( $(this).scrollTop() > 0){
+            $('.irArriba').slideDown(300);
+        }else{
+            $('.irArriba').slideUp(300);
+        }
+    });
+
+});
