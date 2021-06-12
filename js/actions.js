5cecf5e692c024f52dcaa2f9b4cb5816ef4f64d2
jhonkophler <juanadriancorso94@gmail.com>
2021-06-12 10:33:51 -0300

    compuestos de img

--- js/actions.js
+++ js/actions.js
@@ -1,26 +1,3 @@
-let ubicacionPrincipal = window.pageYOffset;
-
-window.addEventListener("scroll", function() {
-   let desplazamientoActual = window.pageYOffset;
-   if (ubicacionPrincipal >= desplazamientoActual) {
-      document.getElementsByTagName("nav")[0].style.top = "0px"
-   } else {
-      document.getElementsByTagName("nav")[0].style.top = "-100px"
-   }
-   ubicacionPrincipal = desplazamientoActual;
-})
-
-let ubicacionPrincipal2 = window.pageYOffset;
-
-window.addEventListener("scroll", function() {
-   let desplazamientoActual = window.pageYOffset;
-   if (ubicacionPrincipal2 >= desplazamientoActual) {
-      document.getElementsByClassName("txt-header")[0].style.top = "75px"
-   } else {
-      document.getElementsByClassName("txt-header")[0].style.top = "-100px"
-   }
-   ubicacionPrincipal2 = desplazamientoActual;
-})
 
 let enlacesHeader = document.querySelectorAll(".menu")[0];
 let semaforo = true;
@@ -37,6 +14,8 @@ document.querySelectorAll(".hamburger")[0].addEventListener("click",function(){
    
    enlacesHeader.classList.toggle("menu2")
 })
+
+
 
 
 jQuery(function ($) {
